448571614205fe1471fa14fec34c049486debf7b
PadalaSandeep <sandeep.dm@live.com>
2022-02-14 22:23:03 +0530

    Allure screeshot implementation

--- src/test/java/stepdefinitions/BasePage.java
+++ src/test/java/stepdefinitions/BasePage.java
@@ -1,11 +1,20 @@
 package stepdefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Allure;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utility.Driverholder;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BasePage {
     public WebDriver driver;
@@ -18,8 +27,14 @@ public class BasePage {
     }
 
     @After
-    public void teardown(){
+    public void teardown() {
         Driverholder.getDriver().quit();
+    }
+
+    @AfterStep
+    public void screenshot(Scenario scenario) throws IOException {
+        File screenshotAs = ((TakesScreenshot) Driverholder.getDriver()).getScreenshotAs(OutputType.FILE);
+        Allure.addAttachment(scenario.getName(), FileUtils.openInputStream(screenshotAs));
     }
 
 }
